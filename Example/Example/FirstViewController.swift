0d8ac9905298d2718932048adc4a18f8bb959f7b
Thomas Gilbert <thomas.gilbert@alexandra.dk>
2017-05-28 21:13:23 +0200

    Update to latest version of xcode

--- Example/Example/FirstViewController.swift
+++ Example/Example/FirstViewController.swift
@@ -23,7 +23,7 @@ class FirstViewController: UIViewController {
     @IBAction func doSupport(_ sender: UIButton) {
         RemoteControllableApp.sharedInstance.isConnected() ?
             RemoteControllableApp.sharedInstance.stopConnection() :
-            RemoteControllableApp.sharedInstance.startConnection("http://testserver:8006", uuid: "My UNIQUE ID")
+            RemoteControllableApp.sharedInstance.startConnection("http://localhost:8006", uuid: "SOMEID")
     }
 }
 
